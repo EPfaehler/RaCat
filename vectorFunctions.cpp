31f6fe0f6a2e79ab12c1704430560ee50578db0f
ellipfaehlerUMCG <e.a.g.pfaehler@umcg.nl>
2018-12-20 19:38:26 +0100

    Delete vectorFunctions.cpp

--- vectorFunctions.cpp
+++
@@ -1,6 +0,0 @@
-//template<typename T>
-void getGreaterElements(vector<float> &vectorOfMatrixElem, float &value){
-    typename vector<float>::iterator greaterThan;
-    greaterThan = remove_if(vectorOfMatrixElem.begin(), vectorOfMatrixElem.end(), bind2nd(greater<float>(), value));
-    vectorOfMatrixElem.erase(greaterThan, vectorOfMatrixElem.end());
-}
